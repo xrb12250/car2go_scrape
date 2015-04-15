82b69ad53161b390502d78951c7455432576791d
xrb12250 <xrb12250@outlook.com>
2015-04-15 14:34:48 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,4 +3,4 @@ import requests
 
 # Blank Python
 
-r = requests.get('http://www.car2go.com/api/v2.1/vehicles?loc=Washington DC&oauth_consumer_key=track2car&format=json')
+r = requests.get('https://www.car2go.com/api/v2.1/vehicles?loc=berlin&oauth_consumer_key=car2gowebsite&format=json&callback=jQuery17107996752320323139_1429045912642')
